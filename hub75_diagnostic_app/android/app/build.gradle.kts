6d018f35e5ef3e21a1c6079275f178d5b14d0058
FlyingThaCat <95353217+FlyingThaCat@users.noreply.github.com>
2026-08-10 19:34:57 +0700

    TEST BUILD Update Flutter build configuration and compile SDK version

--- hub75_diagnostic_app/android/app/build.gradle.kts
+++ hub75_diagnostic_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.hub75_diagnostic_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
